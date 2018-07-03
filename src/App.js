49d72a75ccddcef57f1654afac9229ecce00fb13
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-07-03 20:06:41 +0600

    After Finishing: 059 Firebase Client Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAel2cNKe7XgFLKwBq0QmxQx1DYADpYRPE',
+      authDomain: 'authentication-dfe29.firebaseapp.com',
+      databaseURL: 'https://authentication-dfe29.firebaseio.com',
+      projectId: 'authentication-dfe29',
+      storageBucket: 'authentication-dfe29.appspot.com',
+      messagingSenderId: '49690979669'
+    });
+  }
   render() {
     return (
       <View>
