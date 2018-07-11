e3d1a134ab969e5485a366aec6cc83080acd3a7e
Ishtiaq Mahmood Amin <imamin@ael.net>
2018-07-11 19:21:56 +0600

    React-Native-DotEnv implementation as per Medium.com recommendation
    https://medium.com/@jameshamann/react-native-and-firebase-part-one-simple-authentication-3b233fff5b3f
    
    "It’s a good idea to use react-native-dotenv so you’re not pushing your live api keys for the world to see. It’s super easy to setup, just create a .env file, add it to your .gitignore file, stick all your env vars in there and import them in where ever you need them."

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
+import {
+  FIREBASE_API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  MESSAGING_SENDER_ID
+} from 'react-native-dotenv';
 import { View } from 'react-native';
 import { Header } from './components/common';
 import LoginForm from './components/LoginForm';
 
 const firebase = require('firebase');
 
-
 class App extends Component {
   
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyAel2cNKe7XgFLKwBq0QmxQx1DYADpYRPE',
-      authDomain: 'authentication-dfe29.firebaseapp.com',
-      databaseURL: 'https://authentication-dfe29.firebaseio.com',
-      projectId: 'authentication-dfe29',
-      storageBucket: 'authentication-dfe29.appspot.com',
-      messagingSenderId: '49690979669'
+      apiKey: FIREBASE_API_KEY,
+      authDomain: AUTH_DOMAIN,
+      databaseURL: DATABASE_URL,
+      projectId: FIREBASE_PROJECT_ID,
+      storageBucket: FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: MESSAGING_SENDER_ID
     });
   }
   render() {
